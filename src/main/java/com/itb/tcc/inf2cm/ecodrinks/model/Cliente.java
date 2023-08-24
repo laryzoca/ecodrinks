a1724f2dceee644cb876782355391a11d6a94a11
Kelvin Novais da Silva <rm86157@estudante.fieb.edu.br>
2023-08-24 09:57:32 -0300

    Criação do Model Cliente

---
+++ src/main/java/com/itb/tcc/inf2cm/ecodrinks/model/Cliente.java
@@ -0,0 +1,22 @@
+package com.itb.tcc.inf2cm.ecodrinks.model;
+
+
+
+
+
+
+public class Cliente {
+	
+	private Long id;
+	private String nome;
+	private String telefone;
+	private String cpf;
+	private String email;
+	private String cep;
+	private String Logradouro;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	
+	
+}
