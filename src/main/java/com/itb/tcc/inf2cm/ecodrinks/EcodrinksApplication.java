d978ebfacf20e9f5c55321d1e0a6cefa77ae8c54
Kelvin Novais da Silva <rm86157@estudante.fieb.edu.br>
2023-08-24 08:56:39 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/inf2cm/ecodrinks/EcodrinksApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2cm.ecodrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcodrinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/inf2cm/ecodrinks/EcodrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2cm.ecodrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcodrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
